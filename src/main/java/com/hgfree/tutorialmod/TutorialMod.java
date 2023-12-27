93145353266bd621b4fa400acd3f80a3739f0718
samueldpackard <samueldpackard@gmail.com>
2023-12-26 23:32:56 -0600

    test comment added

---
+++ src/main/java/com/hgfree/tutorialmod/TutorialMod.java
@@ -0,0 +1,20 @@
+package com.hgfree.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		//added this random comment just to see how github works.
+		LOGGER.info("Hello Fabric world!");
+	}
+}
